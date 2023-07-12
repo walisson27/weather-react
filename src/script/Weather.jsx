a850035fa00fc4fa6f7effa1b41ddf7c7de4236a
walisson27 <walisson_souza7@hotmail.com>
2023-07-12 16:13:11 -0300

    style da pagina

--- src/script/Weather.jsx
+++ src/script/Weather.jsx
@@ -1,7 +1,46 @@
 
+import "../css/style.css"
 const Weather = () => {
     return(
-        <h1>tempo</h1>
+        <body>
+            <div className="container">
+                <div className="form">
+                    <h3>O clima de uma cidade:</h3>
+                    <div className="form-input-container">
+                        <input 
+                          type="text" 
+                          placeholder="Digite o nome da cidade"
+                          id="city-input"
+                          />
+                          <button id="search">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                          </button>
+                    </div>
+                </div>
+                <div id="weather-data">
+                  <h2>
+                    <i className="fa-solid fa-location-dot"></i>
+                    <span id="city">Maceió</span>
+                    <img src="" alt="Bandeira do pais" id="country" />
+                  </h2>
+                  <p id="temperature"><span>38</span>&deg;C</p>
+                  <div id="description-container">
+                    <p id="description">Nublado</p>
+                    <img src="" alt="condições do tempo" id="weather-icon"/>
+                  </div>
+                  <div id="details-container">
+                    <p id="umidity">
+                        <i className="fa-solid fa-droplet"></i>
+                        <span>48%</span>
+                    </p>
+                    <p id="wind">
+                        <i className="fa-solid fa-wind"></i>
+                        <span>3km/h</span>
+                    </p>
+                  </div>
+                </div>
+            </div>
+        </body>
     )
 } 
 
